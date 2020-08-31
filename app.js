1ca5c63bdc4837705c87cf77c2bff4a751112368
ostojics <ostojic.dev@gmail.com>
2020-08-31 17:15:52 +0200

    restyled the buttons and added some of the responsivity

--- app.js
+++ app.js
@@ -13,25 +13,29 @@ const secondNav = () => {
 
 // FUNCTION THAT CHECK WHEN USER HAS SCROLLED FROM THE MAIN NAVIGATION AND DISPLAYS THE SECONDARY ONE
 const toggleSecondaryNav = () => {
-
-    window.onscroll = () => {
-        const primaryNav = document.querySelector('.nav');
-        const secondaryNav = document.querySelector('.navigation');
-        const scrollTop = window.pageYOffset;
+    const primaryNav = document.querySelector('.nav');
+    const secondaryNav = document.querySelector('.navigation');
 
 
-        if(scrollTop > primaryNav.offsetHeight){
-            secondaryNav.style.display = 'block';
-        } else {
-            secondaryNav.style.display = 'none';
-        }
-    }
+        window.addEventListener('scroll', () => {
+            // manipulate the secondaryNav element only if the screen size is bigger that 900px 
+            if(screen.width > 900){
+                // check if we scrolled beyond the primaryNav and display the secondaryNav if we did, if not hide the secondaryNav again
+                if(window.scrollY > primaryNav.offsetHeight){
+                    secondaryNav.style.display = 'block';
+                } else {
+                   secondaryNav.style.display = 'none';
+                }
+            }
+       })
+
+
 }
 
 // FUNCTION THAT CONTAINS ALL THE FUNCTIONALITY IN THE APP
 const app = () => {
     toggleSecondaryNav();
-    secondNav();
+    secondNav(); 
 }
 
 
